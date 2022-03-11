91744704a009a5a8c7624b0201f312e9cc752cb3
favour-dev <f.ebebeinwe@gmail.com>
2022-03-11 06:46:15 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
+
 printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
 return (0);
+
 }
